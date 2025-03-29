66060f75baffb038f46ea0d8606d4907f20f33d5
Aryanvishwakarma01 <aryanvish86@gmail.com>
2025-03-29 22:40:34 +0530

    changes on design of login and sign up page

--- registration.js
+++ registration.js
@@ -5,10 +5,9 @@ document.getElementById("register-form").addEventListener("submit", function (ev
         username: document.getElementById("username").value,
         firstName: document.getElementById("first-name").value,
         lastName: document.getElementById("last-name").value,
-        dob: document.getElementById("dob").value,
-        phone: document.getElementById("phone").value,
         gender: document.getElementById("gender").value,
-        password: document.getElementById("password").value
+        password: document.getElementById("password").value,
+        cpassword: document.getElementById("cpassword").value
     };
     // const fileInput = document.getElementById("profile-pic");
     // const file = fileInput.files[0]; // Get selected file
@@ -26,9 +25,21 @@ document.getElementById("register-form").addEventListener("submit", function (ev
     //     };
     //     reader.readAsDataURL(file); // Read file as Base64
     // }
+    function validatePassword() {
+        let password = document.getElementById("password").value;
+        let cpassword = document.getElementById("cpassword").value;
 
-    localStorage.setItem(userData.username, JSON.stringify(userData));
-    alert("Registration successful! You can now log in.");
-    window.location.href = "login.html";
+        if (password !== cpassword) {
+            alert("Passwords do not match!");
+            return false; // Prevent form submission
+        } else {
+            if(userData.password==userData.cpassword){
+                localStorage.setItem(userData.username, JSON.stringify(userData));
+                alert("Registration successful! You can now log in.");
+                window.location.href = "login.html";
+                }
+        }
+    }
+validatePassword()
 });
 
